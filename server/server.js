3c744c07184526b2ed93da40ae8f63bf2e973021
Andrew Eckart <andrew.g.eckart@gmail.com>
2017-02-25 18:53:31 -0500

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const fs = require('fs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -8,6 +10,19 @@ var {User} = require('./models/user');
 var app = express();
 
 app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   var now = new Date().toString();
+//   var log = `${now}: ${req.method} ${req.url}`;
+//
+//   console.log(log);
+//   fs.appendFile('./server/logs/server.log', log + '\n', (err) => {
+//     if (err) {
+//       console.log('Unable to append to server.log');
+//     }
+//   });
+//   next();
+// });
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -27,6 +42,22 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(500).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
+
 });
 
 app.listen(3000, () => {
